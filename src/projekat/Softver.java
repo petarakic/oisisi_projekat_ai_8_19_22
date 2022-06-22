47d394dfab85958dce9b7220f82a0c21dee49c8f
jelena-stanic <Win10@DESKTOP-GC40GRH>
2022-06-22 17:38:32 +0200

    Dodat getter za atribut naziv

--- src/projekat/Softver.java
+++ src/projekat/Softver.java
@@ -4,6 +4,10 @@ public class Softver {
 	
 	public String Naziv;
 	public Cetkica Cetkice;
+	public String FajlFormat;
+	public String Alati;
+	public Render Renderi;
+
 	public Softver(String naziv, Cetkica cetkice, String fajlFormat, String alati, Render renderi) {
 		super();
 		Naziv = naziv;
@@ -12,13 +16,18 @@ public class Softver {
 		Alati = alati;
 		Renderi = renderi;
 	}
-	public String FajlFormat;
-	public String Alati;
-	public Render Renderi;
+	
 	@Override
 	public String toString() {
 		return Naziv +" "+ FajlFormat+" " + Alati+" "+Cetkice.toString()+" " +Renderi.toString();
 	}
 
+	public String getNaziv() {
+		return Naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		Naziv = naziv;
+	}
 
 }
